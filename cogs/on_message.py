2bda68bf21957269afdc8917faf32bb9c23def76
Huyu2239 <61175422+Huyu2239@users.noreply.github.com>
2021-03-19 01:35:34 +0900

    Update on_message.py

--- cogs/on_message.py
+++ cogs/on_message.py
@@ -1,39 +1,34 @@
-import discord
 from discord.ext import commands
-
-lists=[
-    721270505621028926,
-    689098603871862833,
-    730043491626778775,
-    730043630626144298,
-    726656130654273636
+from dispander import dispand
+lists = [
+    689098603871862833
 ]
 
-contents = {
-    "寝なさい": ["眠い", "ねむい"],
-    "お疲れ様です": ["つかれ", "疲れ"],
-    "買いなさい（強制）": ["Adobe","adobe","あどび","アドビ"],
-    "編集しなさい":["進捗","進歩"]
-}
 
 class On_message(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
-    @commands.guild_only()  
+
+    @commands.guild_only()
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.author.id==721265051729657879:
+        if message.author is self.bot.user:
             return
-        if message.channel.id==721270505621028926:
-            await self.bot.get_channel(689098603871862833).send(message.content)
-        if message.channel.id not in lists:
-            return
-        for key, item in contents.items():
-            for i in item:
-                if i in message.content:
-                    await message.channel.send(key)
-                    break
+        if message.guild.id in self.bot.expand:
+            await dispand(message)
+        if message.channel.id in lists:
+            if "眠い" in message.content or "ねむい" in message.content:
+                await message.channel.send("寝なさい")
+                return
+            if "お疲れ" in message.content or "おつかれ" in message.content:
+                await message.channel.send("お疲れ様です")
+                return
+            if "Adobe" in message.content or "アドビ" in message.content or "adobe" in message.content:
+                await message.channel.send("買いなさい（強制）")
+                return
+            if "進歩" in message.content or "進捗" in message.content:
+                await message.channel.send("編集しなさい")
+                return
 
 
 def setup(bot):
