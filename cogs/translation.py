308dee772352317930f0c7d55e7a765de9123613
Huyu2239 <61175422+Huyu2239@users.noreply.github.com>
2021-03-19 01:38:06 +0900

    Update translation.py

--- cogs/translation.py
+++ cogs/translation.py
@@ -1,27 +1,27 @@
-import discord
 from discord.ext import commands
-from googletrans import Translator
-    
+import async_google_trans_new
+
+
 class Translation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
+        self.trans = async_google_trans_new.google_translator()
+
     @commands.Cog.listener()
-    async def on_reaction_add(self,reaction, user):
+    async def on_reaction_add(self, reaction, user):
         if reaction.emoji == '\N{REGIONAL INDICATOR SYMBOL LETTER J}\N{REGIONAL INDICATOR SYMBOL LETTER P}':
-            translator = Translator()
-            trans_ja = translator.translate(reaction.message.content, dest='ja')
+            trans_ja = self.trans.translate(reaction.message.content, dest='ja')
             await reaction.message.channel.send(trans_ja.text)
             return
         if reaction.emoji == '\N{REGIONAL INDICATOR SYMBOL LETTER U}\N{REGIONAL INDICATOR SYMBOL LETTER S}':
-            translator = Translator()
-            trans_en = translator.translate(reaction.message.content, dest='en')
+            trans_en = self.trans.translate(reaction.message.content, dest='en')
             await reaction.message.channel.send(trans_en.text)
             return
         if reaction.emoji == '\N{REGIONAL INDICATOR SYMBOL LETTER D}\N{REGIONAL INDICATOR SYMBOL LETTER E}':
-            translator = Translator()
-            trans_de = translator.translate(reaction.message.content, dest='de')
+            trans_de = self.trans.translate(reaction.message.content, dest='de')
             await reaction.message.channel.send(trans_de.text)
             return
+
+
 def setup(bot):
     bot.add_cog(Translation(bot))
